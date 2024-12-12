eebabbe2ed91a091cb4551a4e41b8ce5fb32a845
Konrad Michalik <kmichalikk19@gmail.com>
2024-12-12 18:34:05 +0100

    Day 12 complete

--- day12/solution.go
+++ day12/solution.go
@@ -8,7 +8,7 @@ type Day12 struct {
 }
 
 type region struct {
-	area, perimeter int
+	area, metric int
 }
 
 type vector struct {
@@ -54,28 +54,28 @@ func (d *Day12) SolveSimple() string {
 
 				n := vector{u.x, u.y - 1}
 				if d.area[u.y][u.x] != d.area[u.y-1][u.x] {
-					r.perimeter++
+					r.metric++
 				} else {
 					q = append(q, n)
 				}
 
 				s := vector{u.x, u.y + 1}
 				if d.area[u.y][u.x] != d.area[u.y+1][u.x] {
-					r.perimeter++
+					r.metric++
 				} else {
 					q = append(q, s)
 				}
 
 				w := vector{u.x - 1, u.y}
 				if d.area[u.y][u.x] != d.area[u.y][u.x-1] {
-					r.perimeter++
+					r.metric++
 				} else {
 					q = append(q, w)
 				}
 
 				e := vector{u.x + 1, u.y}
 				if d.area[u.y][u.x] != d.area[u.y][u.x+1] {
-					r.perimeter++
+					r.metric++
 				} else {
 					q = append(q, e)
 				}
@@ -87,12 +87,101 @@ func (d *Day12) SolveSimple() string {
 
 	total := 0
 	for _, r := range regions {
-		total += r.area * r.perimeter
+		total += r.area * r.metric
 	}
 
 	return fmt.Sprintf("%d\n", total)
 }
 
 func (d *Day12) SolveAdvanced() string {
-	return ""
+	regions := make([]region, 0)
+	visited := make(map[vector]bool)
+
+	for i := 1; i < d.height+1; i++ {
+		for j := 1; j < d.width+1; j++ {
+			v := vector{j, i}
+			if visited[v] {
+				continue
+			}
+
+			q := []vector{v}
+			r := region{}
+			for len(q) > 0 {
+				u := q[0]
+				q = q[1:]
+				if visited[u] {
+					continue
+				}
+
+				r.area++
+				visited[u] = true
+				outsideCount := 0
+
+				n := vector{u.x, u.y - 1}
+				if d.area[u.y][u.x] != d.area[u.y-1][u.x] {
+					outsideCount++
+				} else {
+					q = append(q, n)
+				}
+
+				s := vector{u.x, u.y + 1}
+				if d.area[u.y][u.x] != d.area[u.y+1][u.x] {
+					outsideCount++
+				} else {
+					q = append(q, s)
+				}
+
+				w := vector{u.x - 1, u.y}
+				if d.area[u.y][u.x] != d.area[u.y][u.x-1] {
+					outsideCount++
+				} else {
+					q = append(q, w)
+				}
+
+				e := vector{u.x + 1, u.y}
+				if d.area[u.y][u.x] != d.area[u.y][u.x+1] {
+					outsideCount++
+				} else {
+					q = append(q, e)
+				}
+
+				below := d.area[u.y][u.x]
+				switch outsideCount {
+				case 4:
+					r.metric += 8
+				case 3:
+					r.metric += 4
+				case 2:
+					if (d.area[u.y][u.x+1] != below && d.area[u.y+1][u.x] != below) ||
+						(d.area[u.y][u.x-1] != below && d.area[u.y+1][u.x] != below) ||
+						(d.area[u.y][u.x-1] != below && d.area[u.y-1][u.x] != below) ||
+						(d.area[u.y][u.x+1] != below && d.area[u.y-1][u.x] != below) {
+						r.metric += 2
+					}
+				}
+				if d.area[u.y][u.x+1] == below && d.area[u.y+1][u.x] == below && d.area[u.y+1][u.x+1] != below {
+					r.metric += 2
+				}
+				if d.area[u.y][u.x-1] == below && d.area[u.y+1][u.x] == below && d.area[u.y+1][u.x-1] != below {
+					r.metric += 2
+				}
+				if d.area[u.y][u.x-1] == below && d.area[u.y-1][u.x] == below && d.area[u.y-1][u.x-1] != below {
+					r.metric += 2
+				}
+				if d.area[u.y][u.x+1] == below && d.area[u.y-1][u.x] == below && d.area[u.y-1][u.x+1] != below {
+					r.metric += 2
+				}
+			}
+
+			r.metric /= 2
+			regions = append(regions, r)
+		}
+	}
+
+	total := 0
+	for _, r := range regions {
+		total += r.area * r.metric
+	}
+
+	return fmt.Sprintf("%d\n", total)
 }
